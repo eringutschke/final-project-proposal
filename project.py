b95dfffa6f93dd3d43b1e2966f08f058beaa1eca
Erin Gutschke <erinartsketch@gmail.com>
2023-11-09 20:40:11 -0600

    Created repo, base files, etc

---
+++ project.py
@@ -0,0 +1,18 @@
+def main():
+    ...
+
+
+class SomeClass():
+
+    def method_1():
+        ...
+
+    def method_2():
+        ...
+
+    def method_n():
+        ...
+
+
+if __name__ == "__main__":
+    main()
